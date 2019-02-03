2677aa7a43aee3975bbd79e762544ff286ec204b
Sherief Farid <sherief@farid.cloud>
2019-02-03 17:32:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {};
 
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
