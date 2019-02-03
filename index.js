aab88fbf98bbe3f63312e2fac9fd3d07ebc5a92e
Sherief Farid <sherief@farid.cloud>
2019-02-03 17:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete[key]
+}
